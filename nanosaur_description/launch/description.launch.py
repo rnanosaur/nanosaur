51a18330ae949c890d08e694c2dba6f9fb7cd49b
Raffaello Bonghi <raffaello@rnext.it>
2021-01-01 21:25:24 +0000

    Improve NanoSaur URDF

--- nanosaur_description/launch/description.launch.py
+++ nanosaur_description/launch/description.launch.py
@@ -31,28 +31,26 @@ import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='nanosaur_description').find('nanosaur_description')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf.rviz')
+    default_model_path = os.path.join(pkg_share, 'urdf/nanosaur.urdf.xml')
 
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                              description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                             description='Absolute path to rviz config file'),
-        joint_state_publisher_gui_node,
-        rviz_node
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                             description='Absolute path to robot urdf file'),
+        joint_state_publisher_node,
+        robot_state_publisher_node,
     ])
 # EOF
